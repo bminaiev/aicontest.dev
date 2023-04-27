99db465892109594d84521280189aad1274d8cef
Borys Minaiev <mb.787788@gmail.com>
2023-04-27 01:15:37 +0100

    use release

--- prod/start_client.sh
+++ prod/start_client.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ..
-RUST_LOG=info cargo run --bin example-client -- --num-bots 50 --addr 188.166.195.142:7877
+RUST_LOG=info cargo run --release --bin example-client -- --num-bots 50 --addr 188.166.195.142:7877
