9a0bebac364be51655a5a5d114b7cd64a9d3b21a
Borys Minaiev <bminaiev@gmail.com>
2023-05-01 21:46:24 +0000

    update script

--- prod/start_server.sh
+++ prod/start_server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+set -e
 cd ..
+cargo build --release --bin game-server
 killall game-server
 setsid cargo run --release --bin game-server > prod/server.log 2>&1 &
 tail -f prod/server.log
