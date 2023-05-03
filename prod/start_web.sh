4f7930553f1dcb4950d3001e79e6950f5d97a79b
Borys Minaiev <mb.787788@gmail.com>
2023-05-03 19:44:57 +0100

    use secure connection!

--- prod/start_web.sh
+++ prod/start_web.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ../web
 killall trunk
-SERVER_URL=ws://188.166.195.142:7878 setsid trunk serve --release --address 0.0.0.0 --ignore . > ../prod/web.log 2>&1  &
+SERVER_URL=wss://aicontest.dev:7879/ setsid trunk serve --release --address 0.0.0.0 --ignore . > ../prod/web.log 2>&1  &
 tail -f ../prod/web.log
